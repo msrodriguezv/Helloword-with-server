9f0df1aeb9529d67a1829b9c12b4e94dd8dd47c2
Salome <rodrimartha18@gmail.com>
2025-11-09 21:23:50 -0500

    update host server

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-
-  const express = require('express');
+const express = require('express');
 const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
 
 app.use(express.static(path.join(__dirname, '/')));
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor escuchando en http://${HOST}:${PORT}`);
 });
